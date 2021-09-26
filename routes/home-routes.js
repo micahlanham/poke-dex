a2e0ed945a623e7d6671c21004a7e0d5fe0338cb
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:19:21 -0400

    Update home-routes.js

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Pokemon, } = require('../models');
+const sequelize = require('../config/connection/');
+const { Pokemon, } = require('../models/');
 
 // to do display all pokemon for homepage
 

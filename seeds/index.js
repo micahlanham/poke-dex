332177a60c0fa6c35de6f549d467f920d06278ec
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:20:50 -0400

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
-const seedUsers = require('./user-seeds');
-const seedPokemon = require('./pokemon-seeds');
+const seedUsers = require('./user-seeds/');
+const seedPokemon = require('./pokemon-seeds/');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection/');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });

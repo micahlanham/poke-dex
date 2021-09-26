046da45ed2c841ad6d3a1d8a6666a5b278d496d2
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:17:11 -0400

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const pokemonRoutes = require('./pokemon-routes');
+const pokemonRoutes = require('./pokemon-routes/');
 
-router.use('/pocket monsters', pokemonRoutes);
+router.use('/pocket monsters/', pokemonRoutes);
 
 module.exports = router;

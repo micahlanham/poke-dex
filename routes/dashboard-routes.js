25ec32e15c6420d42b3bd43100cae99e492133f0
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:18:50 -0400

    Update dashboard-routes.js

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Pokemon, User } = require('../models');
-const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection/');
+const { Pokemon, User } = require('../models/');
+const withAuth = require('../utils/auth/');
 
-// display favorited pokemon
 
 
 module.exports = router;

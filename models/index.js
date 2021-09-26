a047e481819eecbf7cf9f646e2f6860be174be28
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-09-26 00:30:40 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,20 +1,8 @@
 const Pokemon = require('./Pokemon');
-const User = require('./User');
-const UserPokemon = require('./UserPokemon');
 
-// Associations
-User.belongsToMany(Pokemon, {
-  foreignKey: 'user_id',
-  through: UserPokemon
-});
 
-Pokemon.belongsToMany(User, {
-  foreignKey: 'pokemon_id',
-  through: UserPokemon
-});
+
 
 module.exports = {
   Pokemon,
-  User,
-  UserPokemon
 };
